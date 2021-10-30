4dabc3f06ba2d23856c511f0c61ca16b48d1f852
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2021-10-30 14:09:20 +0200

    chore: reversed checks

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  get '/check.txt', to: proc {[200, {}, ['it_works']]} # for dokku
   get '/', to: 'index#show'
   get '/metrics', to: 'index#metrics'
   get '/reset', to: 'index#reset'
   get '/:slug', to: 'index#show'
-  get '/check.txt', to: proc {[200, {}, ['it_works']]} # for dokku
 end
