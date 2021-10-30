653e2c29fd5f4ee1372f99fbd8921348370819ac
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2021-10-30 14:05:00 +0200

    chore: config change again and checks

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/metrics', to: 'index#metrics'
   get '/reset', to: 'index#reset'
   get '/:slug', to: 'index#show'
+  get '/check.txt', to: proc {[200, {}, ['it_works']]} # for dokku
 end
